50704c2e6950cd159132de0316539fa70de8563a
The Creative Spark <jnani@duck.com>
2024-07-18 19:29:25 +0530

    rm g++

--- rexarhinscpt/non-root-install.sh
+++ rexarhinscpt/non-root-install.sh
@@ -16,9 +16,9 @@ fi
 read -p "Do you want to Setup Desktop? (y/n): " uin
 uin=$(echo "$uin" | tr '[:upper:]' '[:lower:]')
 if [ "$uin" = "y" ]; then
-  sudo pacman -Syu xorg xorg-xinit i3 feh gcc g++ python3 pulseaudio curl htop neofetch thunar pipewire vlc lightdm firefox alsa-utils i3status dmenu
+  sudo pacman -Syu xorg xorg-xinit i3 feh gcc python3 pulseaudio curl htop neofetch thunar pipewire vlc lightdm firefox alsa-utils i3status dmenu
   sudo systemctl enable lightdm
-  sudo systemctl enable --now pulseaudio
+  sudo systemctl enable pipewire
   touch ~/.xinitrc
   echo "exec i3" >> ~/.xinitrc
   mkdir ~/.config/
