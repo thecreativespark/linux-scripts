552934f458a16a7f78ae47f0666f1f8cbbf0045f
The Creative Spark <jnani@duck.com>
2024-07-18 19:24:19 +0530

    some changes in pacman arguements

--- rexarhinscpt/non-root-install.sh
+++ rexarhinscpt/non-root-install.sh
@@ -16,7 +16,7 @@ fi
 read -p "Do you want to Setup Desktop? (y/n): " uin
 uin=$(echo "$uin" | tr '[:upper:]' '[:lower:]')
 if [ "$uin" = "y" ]; then
-  sudo pacman -Syu xorg xorg-xinit i3 feh gcc g++ python3 pulseaudio curl htop neofetch thunar pipewire vlc lightdm-gtk-greeter firefox alsa-utils alsamixer i3status dmenu
+  sudo pacman -Syu xorg xorg-xinit i3 feh gcc g++ python3 pulseaudio curl htop neofetch thunar pipewire vlc lightdm firefox alsa-utils i3status dmenu
   sudo systemctl enable lightdm
   sudo systemctl enable --now pulseaudio
   touch ~/.xinitrc
